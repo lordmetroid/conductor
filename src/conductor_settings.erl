54c9da6b1e3adcd7e9dba780f82367ac331b05cc
lordmetroid <lordmetroid@yahoo.com>
2013-01-03 09:30:01 +0100

    Initial Settings manager

---
+++ src/conductor_settings.erl
@@ -0,0 +1,64 @@
+-module(conductor_settings).
+-author('Nicklas W Bjurman <nicklas.w.bjurman@monkeyfactory.org').
+
+-behavior(gen_server).
+-export([
+	init/1,
+	handle_call/3,
+	handle_cast/2,
+	handle_info/2,
+	terminate/2,
+	code_change/3
+]).
+
+-export([
+	start_link/0,
+	get/1
+]).
+
+%% ----------------------------------------------------------------------------
+% @doc Read all settings
+%% ----------------------------------------------------------------------------
+init(_Arguments) ->
+	case init:get_argument(conf) of
+		{ok, [Filename]} ->
+			%% Get settings provided by a configuration file
+			file:consult(Filename);
+		error ->
+			%%TODO: Use default settings
+			{ok, []}
+	end.
+
+handle_call({get, Parameter}, _From, Settings) ->
+	case proplists:get_value(Parameter, Settings) of
+		undefined ->
+			{reply, undefined, Settings};
+		Value ->
+			{reply, Value, Settings}
+	end;
+
+handle_call(_Event, _From, State) ->
+	{stop, State}.
+
+handle_cast(_Event, State) ->
+	{stop, State}.
+
+handle_info(_Information, State) ->
+	{stop, State}.
+
+terminate(_Reason, _State) ->
+	ok.
+
+code_change(_OldVersion, State, _Extra) ->
+	{ok, State}.
+
+%% ----------------------------------------------------------------------------
+% @spec start() ->
+% @doc Start the settings manager 
+% ----------------------------------------------------------------------------
+start_link() ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+get(Parameter) ->
+	gen_server:call(?MODULE, {get, Parameter}).
+
