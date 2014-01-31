2f39ff3e031ee61c8de5653611c10b1b451cc3fb
Nicklas W Bjurman <lordmetroid@yahoo.com>
2014-01-31 15:15:41 +0100

    Added make component functions

--- src/conductor_compiler.erl
+++ src/conductor_compiler.erl
@@ -1,17 +1,28 @@
 -module(conductor_compiler).
 
 -export([
-	compile/0
+	make_programs/1,
+	make_models/1
+	make_views/1,
+	make_controllers/1
 ]).
 
+make_programs(Path) ->
+	
+make_models(Path) ->
+	
+make_views(Path) ->
+		
+make_controllers(Path) ->
+	
+
 %% ----------------------------------------------------------------------------
-% @spec compile() -> 
+% @spec module_id() -> UUID::string()
 % @doc Compile half an erlang file into a random module
 %% ----------------------------------------------------------------------------
-compile() ->
+module_id() ->
+	uuid:uuid_to_string(uuid:get_v4()).
 
 	erl_syntax:attribute(erl_syntax:atom(module), [
-		%% Module id
-		erl_syntax:atom(uuid:uuid_to_string(uuid:get_v4(), nodash))
+		erl_syntax:atom(module_id())
 	]).
-
