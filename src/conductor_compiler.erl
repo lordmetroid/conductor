d7e08d00f0a695a0c6e58cd86096bd52b6609356
Nicklas W Bjurman <lordmetroid@yahoo.com>
2014-05-27 10:30:06 +0200

    Added module identification

--- src/conductor_compiler.erl
+++ src/conductor_compiler.erl
@@ -4,8 +4,10 @@
 	make/1
 ]).
 
-
-
+%% ----------------------------------------------------------------------------
+% @spec make(Paths) -> [{Filename, calendar:datetime(), ModuleId}, ...]
+% @doc Compile a modules from specified file paths
+%% ----------------------------------------------------------------------------
 make(Paths) ->
 	make(Paths, []).
 
@@ -13,18 +15,30 @@ make([], Modules) ->
 	%% Return compiled modules
 	Modules;
 make([Path | Rest], Modules) ->
-	%% create a module from source file
+	%% Compile a module from source file
 	make(Rest, [make_module(Path) | Modules]).
 
 make_module(Path) ->
-	%% Add dynamic module id
+	%% Get module location, filename and timestamp
+	ModuleLocation = filename:dirname(Path),
+	ModuleFilename = filename:basename(Path),
+	ModuleModified = filelib:last_modified(Path),
+
+	%% Generate a dynamic module id
 	ModuleId = generate_module_id(),
 	add_module_id(ModuleId).
-	
-	%% TODO: Identify module type
 
-	%% TODO: Add controller standard helper function
-
+	%% Identify module type based on location
+	case ModuleLocation of
+		conductor_settings:get(program_path) ->
+			%% Compile a program module
+		conductor_settings:get(model_path) ->
+			%% Compile a model module
+		conductor_settings:get(view_path) ->
+			%% Compile a view module
+		conductor_settings:get(controller_path) ->
+			%% Compile a controller module
+	end.
 %% ----------------------------------------------------------------------------
 % Compilation helper functions
 %% ----------------------------------------------------------------------------
@@ -47,7 +61,6 @@ add_module_id(ModuleId) ->
 	
 add_model_call(Model, function, Arguments) ->
 	ok.
-
 add_view_call(View, Arguments) ->
 	ok.
 add_controller_call(Controller, function, Arguments) ->
