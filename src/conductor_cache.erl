07768b66f7e43961249eff5dfcd69dbf4bdae4f4
Nicklas W Bjurman <lordmetroid@yahoo.com>
2014-05-30 14:34:19 +0200

    Fixed cache to be nothing but cache

--- src/conductor_cache.erl
+++ src/conductor_cache.erl
@@ -1,4 +1,4 @@
--module(conductor_cache).
+-module(conductor_execute).
 
 -behavior(gen_server).
 -export([
@@ -12,10 +12,10 @@
 
 -export([
 	start_link/0,
-	execute_program/1,
-	execute_model/1,
-	execute_view/1,
-	execute_controller/1
+	get_program/1,
+	get_model/1,
+	get_view/1,
+	get_controller/1
 ]).
 
 %% ----------------------------------------------------------------------------
@@ -39,22 +39,22 @@ init(_Arguments) ->
 		])))
 	}}.
 
-handle_call({execute_program, ProgramName}, _From, Cache) ->
+handle_call({get_program, ProgramName}, _From, Cache) ->
 	{Programs,_,_,_} = Cache,
 	%% TODO: Check if file is updated and update cache
 	proplists:get_value(ProgramName, Programs);
 
-handle_call({execute_model, ModelName}, _From, Cache) ->
+handle_call({get_model, ModelName}, _From, Cache) ->
 	{_,Models,_,_} = Cache,
 	%% TODO: Check if file is updated and update cache
 	proplists:get_value(ModelName, Models);
 	
-handle_call({execute_view, ViewName}, _From, Cache) ->
+handle_call({get_view, ViewName}, _From, Cache) ->
 	{_,_,Views,_} = Cache,
 	%% TODO: Check if file is updated and update cache
 	proplists:get_value(View, Views);
 
-handle_call({execute_controller, ControllerName}, _From, Cache) ->
+handle_call({get_controller, ControllerName}, _From, Cache) ->
 	{_,_,_,Controllers} = Cache,
 	%% TODO: Check if file is updated and update cache
 	proplists:get_value(ControllerName, Controllers);
@@ -76,20 +76,20 @@ code_change(_OldVersion, State, _Extra) ->
 
 %% ----------------------------------------------------------------------------
 % @spec start() ->
-% @doc Start the settings manager 
+% @doc Start the web application file cache manager
 %% ----------------------------------------------------------------------------
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-execute_program(ProgramName) ->
-	gen_server:call(?MODULE, {execute_program, ProgramName}).
-
-execute_model(ModelName) ->
-	get_server:call(?MODULE, {execute_model, ModelName}).
-	
-execute_view(ViewName) ->
-	get_server:call(?MODULE, {execute_view, ViewName}).
-
-execute_controller(ControllerName) ->
-	get_controller:call(?MODULE, {execute_controller, ControllerName}).
-
+%% ----------------------------------------------------------------------------
+% @spec get_...() ->
+% @doc Get the cached web application file
+%% ----------------------------------------------------------------------------
+get_program(ProgramName) ->
+	gen_server:call(?MODULE, {get_program, ProgramName}).
+get_model(ModelName) ->
+	get_server:call(?MODULE, {get_model, ModelName}).
+get_view(ViewName) ->
+	get_server:call(?MODULE, {get_view, ViewName}).
+get_controller(ControllerName) ->
+	get_controller:call(?MODULE, {get_controller, ControllerName}).
