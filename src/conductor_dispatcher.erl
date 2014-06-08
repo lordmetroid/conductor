845722bd3138d4209448504ec9107e7d58bb3dad
lordmetroid <lordmetroid@yahoo.com>
2014-06-08 14:58:09 +0200

    Added resource exists

--- src/conductor_dispatcher.erl
+++ src/conductor_dispatcher.erl
@@ -3,6 +3,7 @@
 -export([
 	init/1,
 	service_available/2,
+	resource_exists/2,
 	options/2,
 	content_types_provided/2,
 	provide_content/2
@@ -15,16 +16,29 @@ init(_Configurations) ->
 	conductor_response:start().
 
 service_available(Request, Response) ->
-	%% Compile response by exceuting request
+	%% Exceute request
 	conductor_router:execute(Request, Response),
 
-	case conductor_response:get_http_status(Response) of
+	%% Check HTTP Status Code
+	case conductor_response:get_status(Response) of
 		500 ->
-			{{error, }Request, Response};
+			%% "500 Internal Server Error"
+			{{error, conductor_log:get(Log)}, Request, Response};
 		503 ->
+			%% "503 Service Unavailable"
 			{false, Request, Response};
 		_ ->
 			{true, Request, Response}
+	end.
+
+resource_exists(Request, Response) ->
+	case conductor_response:get_status(Response) of
+		404 ->
+			%% Requested resource does not exists
+			{false, Request, Response};
+		_ ->
+			%% Requested resource is present
+			{true, Request, response}
 	end.
 
 options(Request, Response) ->
