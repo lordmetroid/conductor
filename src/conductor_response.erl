0cbe62d59e297f5f999150db9f8c88358d96b4e9
lordmetroid <lordmetroid@yahoo.com>
2014-06-09 19:35:59 +0200

    Added response functions

--- src/conductor_response.erl
+++ src/conductor_response.erl
@@ -17,13 +17,15 @@ init(_Arguments) ->
 	%% Initalize the session manager
 	{ok, []}.
 
-handle_call({create_file}, From, Sessions) ->
+handle_call(create_file, From, Sessions) ->
 
-handle_call({create_program}, From, Sessions) ->
+handle_call(create_program, From, Sessions) ->
 
-handle_call({set_status_code}, From, Sessions) ->
+handle_call({set_status_code, Status}, From, Sessions) ->
 
-handle_call({add_content}, From, Sessions) ->
+handle_call(get_status_code, From, Sessions) ->
+
+handle_call({add_content, Content}, From, Sessions) ->
 
 handle_call(_Event, _From, State) ->
 	{stop, State}.
@@ -48,13 +50,16 @@ start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 create_file_response() ->
-	gen_server:call(?MODULE, {create_file}).
+	gen_server:call(?MODULE, create_file).
 
 create_program_response() ->
-	gen_server:call(?MODULE, {create_program}).
+	gen_server:call(?MODULE, create_program).
 
 set_status_code(Status) ->
 	gen_server:call(?MODULE, {set_status_code, Status}).
+
+get_status_code() ->
+	gen_server:call(?MODULE, get_status_code
 
 add_content(Content) ->
 	gen_server:call(?MODULE, {add_content, Content}).
