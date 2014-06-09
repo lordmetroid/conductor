6f02aa2238000442325770f597773a9ee5c92d89
lordmetroid <lordmetroid@yahoo.com>
2014-06-09 17:45:38 +0200

    Initial upload

---
+++ src/conductor_session.erl
@@ -0,0 +1,32 @@
+-module(conductor_session).
+
+-behavior(gen_server).
+-export([
+	init/1,
+	handle_call/3,
+	handle_cast/2,
+	handle_info/2,
+	terminate/2,
+	code_change/3
+]).
+
+-export([
+	start_link/0,
+]).
+
+handle_call(_Event, _From, State) ->
+	{stop, State}.
+
+handle_cast(_Event, State) ->
+	{stop, State}.
+
+handle_info(_Information, State) ->
+	{stop, State}.
+
+terminate(_Reason, _State) ->
+	ok.
+
+code_change(_OldVersion, State, _Extra) ->
+	{ok, State}.
+
+
