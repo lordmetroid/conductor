2d42f2196a47e8a121fc8fefafc461382709d20b
lordmetroid <lordmetroid@yahoo.com>
2014-06-22 09:38:39 +0200

    Initial commit of log manager

---
+++ src/conductor_log.erl
@@ -0,0 +1,41 @@
+-module(conductor_log).
+
+-behavior(gen_server).
+-export([
+	init/1,
+	handle_call/3,
+	handle_cast/2,
+	handle_info/2,
+	terminate/2,
+	code_change/3
+]).
+
+-export([
+	start_link/0,
+]).
+init(_Arguments) ->
+	%% Initalize an empty session manager
+	{ok, []}.
+
+handle_call(_Event, _From, State) ->
+	{stop, State}.
+
+handle_cast(_Event, State) ->
+	{stop, State}.
+
+handle_info(_Information, State) ->
+	{stop, State}.
+
+terminate(_Reason, _State) ->
+	ok.
+
+code_change(_OldVersion, State, _Extra) ->
+	{ok, State}.
+
+%% ----------------------------------------------------------------------------
+% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+% @doc Start the response manager 
+% -----------------------------------------------------------------------------
+start_link() ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
