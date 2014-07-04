71aa1211990bcce62f5a224476d34a88c3bce73d
Nicklas W Bjurman <lordmetroid@yahoo.com>
2014-07-04 14:15:08 +0200

    Added settings runtime update routines

--- src/conductor_settings.erl
+++ src/conductor_settings.erl
@@ -21,20 +21,66 @@
 init(_Arguments) ->
 	case init:get_argument(conf) of
 		{ok, [Filename]} ->
-			%% Get settings provided by a configuration file
-			file:consult(Filename);
+			%% Check if configuration file exist
+			case filelib:last_modified(Filename) of
+				0 ->
+					%% Configuration file not found
+					{ok, {[], [], []}};
+				Date ->
+					%% Get settings provided by a configuration file
+					case file:consult(Filename) of
+						{error, Errors} ->
+							%% Could not interpret configuration file
+							{ok, {[], [], []}};
+						{ok, Settings} ->
+							%% Return settings and configuration file
+							{ok, {Settings, Filename, Date}}
+					end
+			end;
 		error ->
-			%% TODO: Use default settings
-			{ok, []}
+			%% -conf Filename was not specified at runtime
+			{ok, {[], [], []}}
 	end.
 
-handle_call({get, Parameter}, _From, Settings) ->
-	%% TODO: Check if file has been updated
+handle_call({get, Parameter}, _From, {Settings, Filename, Date}) ->
+	%% Check if file has been updated
+	case filelib:last_modified(Filename) of
+		0 ->
+			%% File not found, continue to use old settings
+			Value = get_value(Settings, Parameter),
+			{reply, Value, {Settings, Filename, Date}};			
+		Date ->
+			%% Configuration file is not changed, get value from settings
+			Value = get_value(Settings, Parameter),
+			{reply, Value, {Settings, Filename, Date}};
+		NewDate ->
+			%% Configuration file has been updated
+			case file:consult(Filename) of
+				{error, Errors} ->
+					%% Could not interpret updated configuration file
+					conductor_log:add(Filename, "Could not be interpreted"),
+					
+					%% Use old settings until configuration file is corrected
+					Value = get_value(Settings, Parameter),
+					{reply, Value, {Settings, Filename, NewDate}};		
+				{ok, UpdatedSettings} ->
+					%% Get value from updated configuration file
+					Value = get_value(UpdatedSettings, Parameter),
+					{reply, Value, {UpdatedSettings, Filename, Date}}
+	end;
+
+%% ----------------------------------------------------------------------------
+% @spec get_value() -> Value | undefined
+% @doc Get value specified in settings for parameter
+% -----------------------------------------------------------------------------
+get_value(Settings, Parameter) ->
 	case lists:keyfind(Parameter, 1, Settings) of
 		false ->
-			{reply, undefined, Settings};
-		{_, Value} ->
-			{reply, Value, Settings}
+			%% Parameter not found
+			undefined;
+		{Parameter, Value} ->
+			%% Return value
+			Value
 	end;
 
 handle_call(_Event, _From, State) ->
