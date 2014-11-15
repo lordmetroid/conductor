33089e9e285ff2adfe6b0f222624a826b9ded469
Nicklas W Bjurman <lordmetroid@yahoo.com>
2014-11-15 18:17:01 +0100

    Initial commit

---
+++ src/conductor_supervisor_cache.erl
@@ -0,0 +1,15 @@
+-module(director_supervisor_cache).
+
+-behaviour(supervisor).
+-export([
+	init/1
+]).
+
+-export([
+	start_link/0
+]).
+
+init(_Arguments) ->
+
+start_link() ->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, [], []).
