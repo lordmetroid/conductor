1bc9d27a2f842d1b0d6cd12c30ec9deec729bd66
Nicklas W Bjurman <lordmetroid@yahoo.com>
2015-01-15 19:13:26 +0100

    Semantic rewrite

--- src/conductor_application.erl
+++ src/conductor_application.erl
@@ -10,39 +10,24 @@
 % Application callbacks
 %% ----------------------------------------------------------------------------
 
-%% @doc Start Conductor web application platform application
+% @doc Starts the application
+% @spec
 start(_Type, _StartArguments) ->
-	%% Start all application dependencies
-	Applications = [kernel, stdlib, sasl, crypto, inets, mochiweb, webmachine],
-	case start_applications(Applications) of
-		{error, Errors} ->
-			%% Could not start depending application
-			{error, Errors};
-		ok ->
-			%% Start Conductor web application platform application
-			conductor_supervisor:start_link()
-	end.
-	
-%% @doc Stop Conductor web application platform application
+	log_info(conductor_application, starting),
+	start_application_supervisor().
+
+start_application_supervisor() ->
+	conductor_supervisor:start().
+
+% @doc Stops the application
+% @spec
 stop(_State) ->
+	log_info(application, stopping),
+	stop_application().
+
+stop_application() ->
 	stop.
 
-%% ----------------------------------------------------------------------------
-% Internal Functions
-%% ----------------------------------------------------------------------------
-%% @doc Start dependent applications if they are not already started
-start_applications([]) ->
-	%% All applications started
-	ok;
-start_applications([Application | Rest]) ->
-	case application:start(Application) of
-		{error, {already_started, Application}} ->
-			%% Application already started
-			start_applications(Rest);
-		{error, Errors} ->
-			%% Could not start application
-			{error, Errors};
-		ok ->
-			%% Application started
-			start_applications(Rest)
-	end.
+log_info(Operation, Information) ->
+	lager:
+
