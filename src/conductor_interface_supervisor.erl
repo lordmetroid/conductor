a0bb4b55fb9e4dc559a13dcd6c489f3f0a4b232a
Nicklas W Bjurman <lordmetroid@yahoo.com>
2015-05-12 19:34:39 +0200

    Socialized code

--- src/conductor_interface_supervisor.erl
+++ src/conductor_interface_supervisor.erl
@@ -1,4 +1,5 @@
 -module(conductor_interface_supervisor).
+-compile({parse_transform, lager_transform}).
 
 -behaviour(supervisor).
 -export([
@@ -9,42 +10,69 @@
 	start_link/0
 ]).
 
-%% ----------------------------------------------------------------------------
-% Supervisor callbacks
-%% ----------------------------------------------------------------------------
+%% ============================================================================
+%  Supervisor callback functions
+%% ============================================================================
 
-%% ----------------------------------------------------------------------------
-% @spec init(Arguments) -> {ok, Arguments}
-% @doc Initialize a supervisor with the child specifications arguments
-%% ----------------------------------------------------------------------------
+%% @doc
 init(_Arguments) ->
-	%% Load the web-server settings
-	ServerSettings = [
-		%% Server settings
+	{ok, {supervisor_configurations(), supervisor_child_specifications()}}.
+
+supervisor_configurations() ->
+	RestartStrategy = one_for_one,
+	MaxRestarts = 10,
+	MaxRestartsResetTimer = 3600, %% Seconds
+	{RestartStrategy, MaxRestarts, MaxRestartsResetTimer}.
+
+supervisor_child_specifications() ->
+	[
+		conductor_interface()
+	].
+
+conductor_interface() ->
+	Id = conductor_interface,
+	Module = webmachine_mochiweb,
+	Function = start,
+	Arguments = [conductor_interface_configurations()},
+	Restart = permanent,
+	Timeout = 7200, %% Seconds
+	Type = worker,
+	Callback = dynamic,
+	{Id, {Module, Function, Arguments}, Restart, Timeout, Type, Callback}.
+
+conductor_interface_configurations() ->
+	[
+		%% Server configurations
 		{ip, "127.0.0.1"},
 		{port, 8000}, 
 
-		%% Resource dispatcher
+		%% Interfaces
 		{dispatch, [
-			{['*'], conductor_dispatcher, []}
+			{['*'], conductor_router_interface, []}
 		]}
 	],
-
-	{ok, {one_for_one, 10, 3600}, [
-		{webmachine_mochiweb,
-			{webmachine_mochiweb, start, [ServerSettings]},
-			permanent, 5000, worker, dynamic
-		}
-	]}.
 
 %% ----------------------------------------------------------------------------
 % API 
 %% ----------------------------------------------------------------------------
 start_link() ->
-	case whereis(?MODULE) of
-		undefined ->
-			supervisor:start_link({local, ?MODULE}, ?MODULE, [], []);
-		Pid ->
-			{error, {already_started, Pid}}
-	end.
+	Name = {local, conductor_interface_supervisor},
+	Module = conductor_interface_supervisor,
+	Arguments = [],
+
+	Results = supervisor:start_link(Name, Module, Arguments),
+	log_supervisor_init(Results),
+	Results.
+
+%% ============================================================================
+%  Logging functions
+%% ============================================================================
+
+	log_supervisor_init({ok, Pid}) ->
+		lager:info("Started Conductor interface supervisor: ~s", [Pid]);
+	log_supervisor_init(ignore) ->
+		lager:warning("Conductor inteface supervisor returned ignore");
+	log_supervisor_init({error, Reason}) ->
+		lager:error("Could not start Conductor interface supervisor: ~s", [Reason]).
+
 
